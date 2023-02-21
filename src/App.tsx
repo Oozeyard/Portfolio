770ef9745253ab6533dad8834b2fce6f64f1c00c
Oozeyard <mickaellll5@gmail.com>
2023-02-21 15:34:03 +0100

    navbar + css

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,44 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-function App() {
+/*      <div>    
+        <a href="https://reactjs.org" target="_blank">
+         <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div> */
+
+export function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Bienvenue</h1>
+      <div className="presentation">
+        <h2>Je m'appelle Mickaël Canhoto <br/>
+        et je construit des choses sur internet</h2>
+        <h3>Etudiant en informatique</h3>
+    </div>
+      <div id='about'>
+      fe
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div id='project'>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div id='contact'>
+
+      </div>
+      <footer></footer>
+      <Panel/>
     </div>
   )
 }
 
-export default App
+function Panel() {
+  return (
+      <div id="panel">
+        <a href='#about'>A Propos</a>
+        <a href='#project'>Projet</a> 
+        <a href='#contact'>Contact</a>
+      </div>
+  )
+}
